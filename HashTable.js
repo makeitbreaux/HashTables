5668f22765cef03014f716b831c4997e6f5527ae
makeitbreaux <makeitbreaux@gmail.com>
2022-04-07 09:13:05 -0500

    created hash function

--- HashTable.js
+++ HashTable.js
@@ -2,6 +2,16 @@ class HashTable {
     constructor(arrLen=53) {
         this.table = new Array(arrLen);
     }
+    //used an underscore to indicate that the method is private to the class HashTable and should not be used outside of the class
+    _hash(val) {
+        let PRIME = 31;
+        let key = 0;
+        
+        for (let i=0; i < val.length; i++) {
+            key = (key * PRIME + val.charCodeAt(i)) % this.table.length;
+        }
+        return key;
+    }
 }
 
 const ht = new HashTable();
